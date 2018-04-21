29578586bc19ff73743eeab0545de6479588a592
Tyson Bliss <tysonbliss@Tysons-MacBook-Pro.local>
2018-04-21 14:21:48 -0600

    Implemented custom action for updating statuses for blog posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -54,9 +54,18 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully deleted.' }
+      format.html { redirect_to blogs_url, notice: 'Post was successfully deleted.' }
       format.json { head :no_content }
     end
+  end
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published! 
+      @blog.draft!      
+      end  
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
