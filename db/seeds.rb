5bd4f4068a9e22b9f1604080af721459a28caff8
Tyson Bliss <tysonbliss@Tysons-MacBook-Pro.local>
2018-04-29 11:17:43 -0600

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 end
 
 puts "3 topics created"
-    
 
 10.times do |blog|
   Blog.create!(
@@ -17,13 +16,11 @@ end
 
 puts "10 blog post created"
 
-
 5.times do |skill|
   Skill.create!(
     title: "Rails #{skill}" ,
     percent_utilized: 15
     )
-  
 end
 
 puts "5 skills post created"
@@ -39,7 +36,6 @@ puts "5 skills post created"
     main_image: "http://placehold.it/600x400" ,
     thumb_image: "http://placehold.it/350x200" ,
     )
-  
 end
 
 1.times do |portfolio_item|
@@ -53,13 +49,14 @@ end
     main_image: "http://placehold.it/600x400" ,
     thumb_image: "http://placehold.it/350x200" ,
     )
-  
 end
 
-puts "8 portfolio items created"
+puts "9 portfolio items created"
 
+3.times do |technology|
+    Portfolio.last.technologies.create!(
+    name: "Technology #{technology}",
+    )
+end
 
-
-
-
-
+puts "3 technologies created"
