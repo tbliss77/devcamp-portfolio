ce48d19a1ac6272fcb2594461538e96394db328f
Tyson Bliss <tysonbliss@Tysons-MacBook-Pro.local>
2018-05-20 11:31:01 -0600

    Added custom layouts for blogs and portfolios

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -7,6 +7,6 @@ module DefaultPageContent
 
   def set_page_defaults
     @page_title = "Devcamp Portfolio | My Portfolio Website"
-    @seo_keywords = "Tyson Bliss Portfolio"
+    @seo_keywords = "Tyson Bliss' Portfolio"
   end
 end
