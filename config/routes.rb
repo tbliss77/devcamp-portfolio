36504ba259999e41c8b255585b8a68505ada84da
Tyson Bliss <tysonbliss@Tysons-MacBook-Pro.local>
2019-01-07 21:58:07 -0700

    Built out topic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'topics/index'
-
-  get 'topics/show'
-
   resources :topics, only: [:index, :show]
 
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
