c3d16e6df4a40e52e3cf9e229c2c650942e133a3
Tyson Bliss <tysonbliss@Tysons-MacBook-Pro.local>
2019-02-11 22:19:21 -0700

    Updated JS again

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require popper
 //= require jquery3
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
@@ -22,3 +21,4 @@
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
